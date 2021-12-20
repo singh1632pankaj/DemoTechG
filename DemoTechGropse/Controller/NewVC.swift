fb283f92d45f476eb24e9fd297977044e0761bfb
Pankajblulabs <96104016+Pankajblulabs@users.noreply.github.com>
2021-12-20 20:13:07 +0530

    NewVc

---
+++ DemoTechGropse/Controller/NewVC.swift
@@ -0,0 +1,20 @@
+//
+//  NewVC.swift
+//  DemoTechGropse
+//
+//  Created by Pankaj Singh on 20/12/21.
+//
+
+import UIKit
+
+class NewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
